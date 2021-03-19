7561d945632117b883fd921401095276b8e52928
David Vernon <david@vernon.eu>
2021-03-19 08:27:25 +0100

    cameraModel updated to use double and SVD solver

--- module5/src/cameraModel/cameraModelImplementation.cpp
+++ module5/src/cameraModel/cameraModelImplementation.cpp
@@ -32,107 +32,107 @@ void computeCameraModel(int numberOfControlPoints, worldPointType worldPoints[],
    int i, j;
    bool debug = false;
 
-   Mat c(NUMBER_OF_UNKNOWNS,1,CV_32FC1);
-   Mat y(2*numberOfControlPoints,1,CV_32FC1);
-   Mat X(2*numberOfControlPoints,NUMBER_OF_UNKNOWNS,CV_32FC1);
-   Mat Xc(2*numberOfControlPoints,1,CV_32FC1); // X * c for validation ... should equal y
+   Mat c(NUMBER_OF_UNKNOWNS,1,CV_64FC1);
+   Mat y(2*numberOfControlPoints,1,CV_64FC1);
+   Mat X(2*numberOfControlPoints,NUMBER_OF_UNKNOWNS,CV_64FC1);
+   Mat Xc(2*numberOfControlPoints,1,CV_64FC1); // X * c for validation ... should equal y
 
    /* y: vector of image coordinates */
    for (i=0; i<numberOfControlPoints; i++) {
-      y.at<float>(2*i)   = (float) imagePoints[i].u;
-      y.at<float>(2*i+1) = (float) imagePoints[i].v;
+      y.at<double>(2*i)   = (double) imagePoints[i].u;
+      y.at<double>(2*i+1) = (double) imagePoints[i].v;
    }
 
    if (debug) {
       printf("y \n");
       for (i=0; i<2*numberOfControlPoints; i++) {
-         printf("%3.1f ", y.at<float>(i));
+         printf("%3.1f ", y.at<double>(i));
       }
       printf("\n\n");
    }
 
    /* X: matrix */
    for (i=0; i<numberOfControlPoints; i++) {
-      X.at<float>(2*i,0)   = worldPoints[i].x;
-      X.at<float>(2*i,1)   = worldPoints[i].y;
-      X.at<float>(2*i,2)   = worldPoints[i].z;
-      X.at<float>(2*i,3)   = 1.0;
-      X.at<float>(2*i,4)   = 0.0;
-      X.at<float>(2*i,5)   = 0.0;
-      X.at<float>(2*i,6)   = 0.0;
-      X.at<float>(2*i,7)   = 0.0;
-      X.at<float>(2*i,8)   = - imagePoints[i].u * worldPoints[i].x;
-      X.at<float>(2*i,9)   = - imagePoints[i].u * worldPoints[i].y;
-      X.at<float>(2*i,10)  = - imagePoints[i].u * worldPoints[i].z;
+      X.at<double>(2*i,0)   = worldPoints[i].x;
+      X.at<double>(2*i,1)   = worldPoints[i].y;
+      X.at<double>(2*i,2)   = worldPoints[i].z;
+      X.at<double>(2*i,3)   = 1.0;
+      X.at<double>(2*i,4)   = 0.0;
+      X.at<double>(2*i,5)   = 0.0;
+      X.at<double>(2*i,6)   = 0.0;
+      X.at<double>(2*i,7)   = 0.0;
+      X.at<double>(2*i,8)   = - imagePoints[i].u * worldPoints[i].x;
+      X.at<double>(2*i,9)   = - imagePoints[i].u * worldPoints[i].y;
+      X.at<double>(2*i,10)  = - imagePoints[i].u * worldPoints[i].z;
 
-      X.at<float>(2*i+1,0)   = 0.0;
-      X.at<float>(2*i+1,1)   = 0.0;
-      X.at<float>(2*i+1,2)   = 0.0;
-      X.at<float>(2*i+1,3)   = 0.0;
-      X.at<float>(2*i+1,4)   = worldPoints[i].x;
-      X.at<float>(2*i+1,5)   = worldPoints[i].y;
-      X.at<float>(2*i+1,6)   = worldPoints[i].z;
-      X.at<float>(2*i+1,7)   = 1.0;
-      X.at<float>(2*i+1,8)   = - imagePoints[i].v * worldPoints[i].x;
-      X.at<float>(2*i+1,9)   = - imagePoints[i].v * worldPoints[i].y;
-      X.at<float>(2*i+1,10)  = - imagePoints[i].v * worldPoints[i].z;
+      X.at<double>(2*i+1,0)   = 0.0;
+      X.at<double>(2*i+1,1)   = 0.0;
+      X.at<double>(2*i+1,2)   = 0.0;
+      X.at<double>(2*i+1,3)   = 0.0;
+      X.at<double>(2*i+1,4)   = worldPoints[i].x;
+      X.at<double>(2*i+1,5)   = worldPoints[i].y;
+      X.at<double>(2*i+1,6)   = worldPoints[i].z;
+      X.at<double>(2*i+1,7)   = 1.0;
+      X.at<double>(2*i+1,8)   = - imagePoints[i].v * worldPoints[i].x;
+      X.at<double>(2*i+1,9)   = - imagePoints[i].v * worldPoints[i].y;
+      X.at<double>(2*i+1,10)  = - imagePoints[i].v * worldPoints[i].z;
    }
 
    if (debug) {
       printf("X\n");
       for (i=0; i<2*numberOfControlPoints; i++) {
          for (j=0; j<NUMBER_OF_UNKNOWNS; j++) {
-             printf("%3.1f ", X.at<float>(i,j));
+             printf("%3.1f ", X.at<double>(i,j));
          }     
          printf("\n");
       }
       printf("\n");
    }
 
-   solve(X,y,c,DECOMP_QR);  //  DECOMP_SVD
+   solve(X,y,c,DECOMP_SVD);  //  DECOMP_SVD
 
    Xc = X * c;
 
    if (debug) {
       printf("Xc: ");
       for (i=0; i<2*numberOfControlPoints; i++) {
-         printf("%3.1f  ", Xc.at<float>(i));
+         printf("%3.1f  ", Xc.at<double>(i));
       }
       
       printf("\n");
 
       printf("y:  ");
       for (i=0; i<2*numberOfControlPoints; i++) {
-         printf("%3.1f  ", y.at<float>(i));
+         printf("%3.1f  ", y.at<double>(i));
       }
       printf("\n\n");
    }
 
 
-   cameraModel[0][0] = c.at<float>(0);
-   cameraModel[0][1] = c.at<float>(1);
-   cameraModel[0][2] = c.at<float>(2);
-   cameraModel[0][3] = c.at<float>(3);
-   cameraModel[1][0] = c.at<float>(4);
-   cameraModel[1][1] = c.at<float>(5);
-   cameraModel[1][2] = c.at<float>(6);
-   cameraModel[1][3] = c.at<float>(7);
-   cameraModel[2][0] = c.at<float>(8);
-   cameraModel[2][1] = c.at<float>(9);
-   cameraModel[2][2] = c.at<float>(10);
+   cameraModel[0][0] = c.at<double>(0);
+   cameraModel[0][1] = c.at<double>(1);
+   cameraModel[0][2] = c.at<double>(2);
+   cameraModel[0][3] = c.at<double>(3);
+   cameraModel[1][0] = c.at<double>(4);
+   cameraModel[1][1] = c.at<double>(5);
+   cameraModel[1][2] = c.at<double>(6);
+   cameraModel[1][3] = c.at<double>(7);
+   cameraModel[2][0] = c.at<double>(8);
+   cameraModel[2][1] = c.at<double>(9);
+   cameraModel[2][2] = c.at<double>(10);
    cameraModel[2][3] = 1.0;
 
 
    /* check result */
 
    if (false && debug) {
-      float u, v, t;
+      double u, v, t;
       printf("Validation\n");
       for (i=0; i<numberOfControlPoints; i++) {
           printf("Actual:  (%4.1f %4.1f %4.1f) -> (%4d %4d)\n", worldPoints[i].x,  worldPoints[i].y,  worldPoints[i].z, imagePoints[i].u, imagePoints[i].v);
-          u = (float)(cameraModel[0][0]*worldPoints[i].x + cameraModel[0][1]*worldPoints[i].y + cameraModel[0][2]*worldPoints[i].z + cameraModel[0][3]*(float)1.0);
-          v = (float)(cameraModel[1][0]*worldPoints[i].x + cameraModel[1][1]*worldPoints[i].y + cameraModel[1][2]*worldPoints[i].z + cameraModel[1][3]*(float)1.0);
-          t = (float)(cameraModel[2][0]*worldPoints[i].x + cameraModel[2][1]*worldPoints[i].y + cameraModel[2][2]*worldPoints[i].z + cameraModel[2][3]*(float)1.0);
+          u = (double)(cameraModel[0][0]*worldPoints[i].x + cameraModel[0][1]*worldPoints[i].y + cameraModel[0][2]*worldPoints[i].z + cameraModel[0][3]*(double)1.0);
+          v = (double)(cameraModel[1][0]*worldPoints[i].x + cameraModel[1][1]*worldPoints[i].y + cameraModel[1][2]*worldPoints[i].z + cameraModel[1][3]*(double)1.0);
+          t = (double)(cameraModel[2][0]*worldPoints[i].x + cameraModel[2][1]*worldPoints[i].y + cameraModel[2][2]*worldPoints[i].z + cameraModel[2][3]*(double)1.0);
           printf("Computed:(%4.1f %4.1f %4.1f) -> (%5.4f %5.4f %5.4f) -> (%4.1f %4.1f)\n\n", worldPoints[i].x,  worldPoints[i].y,  worldPoints[i].z, u, v, t, u/t, v/t );
       }
       printf("\n");
